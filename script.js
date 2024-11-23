89c2e9ab6a4324a03ef5672c59b20eb74b2c9e41
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2024-11-23 21:02:53 +0000

    Add html content and set font in css

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log(`Hello`);
+
+// window.alert(`This is an alert`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `Hello World!`;
+
+let x = 100;
+
+console.log(x)
